72777ea2ae47f279c50866a95e46f24946eaf292
Godswill Onuoha <Godswill.Onuoha@ng.ey.com>
2018-06-26 14:04:08 +0100

    enable test of Heroku integration

--- server/app.js
+++ server/app.js
@@ -1,38 +1,7 @@
 var express = require("express");
-// var bodyParser = require("body-parser");
-// var path = require("path");
-// var expressValidator = require('express-validator')
-
 var app = express();
 
-// //Body Parser Midleware
-// app.use(bodyParser.urlencoded());
-// app.use(bodyParser.json());
-
-
-//Global variable
-app.use(function(req, res, next){
-	res.locals.errors = null;
-	next();
-})
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.get('/rides', function(req, res){
-	// find everything in rides and show here
-})
-
-app.get('/rides/:id', function(req, res){
-	// find single ride and show here
-})
-
-app.post('/rides', function(req, res){
-	
-})
-
-app.post('/rides/:id/requests', function(req, res){
-	
-})
+app.get('/', (req, res) => res.send('Hello World!'));
 
 const port = process.env.PORT || 3000
 app.listen(port)
