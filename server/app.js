8c3364dd7cca902bc3011619b28fb1ccd240583c
ozezzy <40201026+ozezzy@users.noreply.github.com>
2018-06-28 15:05:05 +0100

    chore(setup): integrate babel and eslint
    [FNISHED #158686547]

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
-var express = require("express");
-var app = express();
+import express from 'express';
+
+const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port);
 
-module.exports = app;
+export default app;
