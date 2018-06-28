c7dc8cd748931b775a4a0892f1c0ebce78d4823a
Godswill Onuoha <Godswill.Onuoha@ng.ey.com>
2018-06-28 07:01:14 +0100

    update badges coverage

--- server/app.js
+++ server/app.js
@@ -3,8 +3,7 @@ var app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-const port = process.env.PORT || 3000;
-app.listen(port, function(){
-	console.log("server started on port 3000 ...")});
+const port = process.env.PORT || 3000
+app.listen(port);
 
 module.exports = app;
