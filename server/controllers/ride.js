157dbd472adf1ddbac5536d4df2ed142e95b6da5
Godswill Onuoha <Godswill.Onuoha@ng.ey.com>
2018-06-29 06:43:56 +0100

    add a get single ride offer api feature

--- server/controllers/ride.js
+++ server/controllers/ride.js
@@ -7,10 +7,27 @@ const getAllRides = (req, res) => {
   const rides = db.getAllRides();
   res.status(200).json({
     message: 'success',
-    Rides: rides,
+    rides,
+  });
+};
+
+
+// Get a single ride
+const getSingleRide = (req, res) => {
+  const id = parseInt(req.params.rideId, 10);
+  const ride = db.getRide(id);
+  if (ride === undefined) {
+    return res.status(404).json({
+      error: `no ride with id - ${id} found`,
+    });
+  }
+  return res.status(200).json({
+    message: 'Sucess!',
+    ride,
   });
 };
 
 export default {
   getAllRides,
+  getSingleRide,
 };
