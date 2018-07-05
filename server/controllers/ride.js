4192fafb36c61406d9c3c67c8057bfd82f7532f5
ozezzy <40201026+ozezzy@users.noreply.github.com>
2018-07-05 12:11:31 +0100

    chore(setup) integrate Postgress database
    [ finishes #58750264 ]

--- server/controllers/ride.js
+++ server/controllers/ride.js
@@ -1,67 +1,96 @@
-import database from '../models/ride';
-
-const db = database;
+import db from '../models/ride';
 
 // returns all rides offers
 const getAllRides = (req, res) => {
-  const rides = db.getAllRides();
-  res.status(200).json({
-    message: 'success',
-    rides,
-  });
+  const result = db.fetchRideOffers();
+  result.then(data => res.status(200).json({
+    message: 'succs',
+    ride: data.rows,
+  }));
 };
 
 // Get a single ride
 const getSingleRide = (req, res) => {
-  const id = parseInt(req.params.rideId, 10);
-  const ride = db.getRide(id);
-  if (ride === undefined) {
-    return res.status(404).json({
-      error: `no ride with id - ${id} found`,
+  const rideId = parseInt(req.params.rideId, 10);
+  const result = db.fetchRideOffer(rideId);
+  result.then((data) => {
+    const ride = data.rows[0];
+
+    if (ride === undefined) {
+      return res.status(404).json({
+        error: 'no ride with id found',
+      });
+    }
+
+    return res.status(200).json({
+      message: 'Sucess!',
+      ride,
     });
-  }
-  return res.status(200).json({
-    message: 'Sucess!',
-    ride,
   });
 };
 
 
 // process request to join ride
 const joinRide = (req, res) => {
-  const id = parseInt(req.params.rideId, 10);
-  const ride = db.getRide(id);
+  const rideId = parseInt(req.params.rideId, 10);
+  const ride = db.fetchRideOffer(rideId);
 
-  if (ride === undefined) {
-    return res.status(404).json({
-      error: `no ride with id - ${id} found`,
-    });
-  }
-  const userIds = [1, 2, 3, 4, 5];
   const requester = {
     userid: req.body.userId,
   };
+  const userIds = [1, 2, 3, 4, 5, 6];
 
-  if (!userIds.includes(parseInt(requester.userid, 10))) {
-    return res.status(400).json({
-      error: 'request user not valid',
+  ride.then((rideRes) => {
+    const userid = parseInt(requester.userid, 10);
+
+    // check ride exists
+    if (!rideRes.rows[0]) {
+      return res.status(400).json({
+        error: 'no ride with id found',
+      });
+    }
+
+    // validate user
+    if (!userIds.includes(userid)) {
+      return res.status(400).json({
+        error: 'request user not valid',
+      });
+    }
+
+    const joinReq = JSON.parse(rideRes.rows[0].joinrequests);
+    joinReq.push(userid);
+
+    // make set entries unique
+    const joinReqSet = new Set(joinReq);
+
+
+    const addRequest = db.addRideRequest(rideId, JSON.stringify(joinReqSet));
+    addRequest.then((writeRes) => {
+      if (writeRes.rowCount) {
+        return res.status(200).json({
+          message: 'success',
+        });
+      }
+      return res.status(500).json({
+        error: 'server error',
+      });
     });
-  }
-  return res.status(200).send({
-    message: 'Ride has been requested',
-    ride: db.updateJoinRequest(id, requester),
+    return res.status(500).json({
+      reeor: 'server error',
+    });
   });
 };
+
 
 // process request to join ride
 const createRide = (req, res) => {
   const newRide = {
-    bStop: req.body.boadingStop,
-    fDestinantion: req.body.finalDestination,
+    boadingStop: req.body.boadingStop,
+    finalDestination: req.body.finalDestination,
     time: req.body.time,
     date: req.body.date,
-    vType: req.body.vehicleType,
-    pStops: req.body.possibleStops,
+    vehicleType: req.body.vehicleType,
+    possibleStops: req.body.possibleStops,
   };
 
   if (!newRide.pStops) {
@@ -70,19 +99,26 @@ const createRide = (req, res) => {
 
   // return error if a field is missing
   // ME: Learn and use express validator next !!!!!!
-  if (!newRide.bStop || !newRide.fDestinantion || !newRide.time
+  if (!newRide.boadingStop || !newRide.finalDestination || !newRide.time
      || !newRide.date) {
     return res.status(400).json({
       error: 'missing fields',
     });
   }
 
-  db.addRide(newRide.bStop, newRide.fDestinantion,
-    newRide.time, newRide.date, newRide.vType, newRide.pStops);
-
-  return res.status(200).json({
-    message: 'Sucess!',
-    rideId: db.ids,
+  const addRideRes = db.createRide(newRide);
+  addRideRes.then((result) => {
+    if (result.rowCount) {
+      return res.status(200).json({
+        message: 'Sucess!',
+      });
+    }
+    return res.status(500).json({
+      error: 'database error!',
+    });
+  });
+  return res.status(500).json({
+    error: 'server error!',
   });
 };
 
